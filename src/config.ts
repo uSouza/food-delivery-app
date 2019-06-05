e8537c7b7eb8aeebeec7039e82fa01c86a654c5d
Uesley S. Silva <uesleysilva@alunos.utfpr.edu.br>
2019-06-05 00:08:27 -0300

    Retirada as configurações e assets privados

--- src/config.ts
+++ src/config.ts
@@ -5,16 +5,14 @@ import { Injectable } from '@angular/core';
 export class Config {
 }
 
-export const sender_id = '825111433849';
-export const oneSignalAppId = '18e4fb1f-4d47-4196-8ded-4883a763d9d7';
+export const sender_id = '';
+export const oneSignalAppId = '';
 export const client_id = 1;
-export const client_secret = 'kss5gCVZ2wQyRuwf5AvfPbGbwo4AjCJ8DsXidza8'
+export const client_secret = ''
 
 //Production
-export const api_url = 'http://api.pandeco.com.br/';
+export const api_url = '';
 
 //Test
 // export const api_url = 'http://localhost:8000/';
 
-//Test
-//export const api_url = 'http://api2.pandeco.com.br/';
